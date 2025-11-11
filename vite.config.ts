c3ed3aea347142fb28431ea7d60f252aae7338df
Leisson1993 <71749120+Leisson1993@users.noreply.github.com>
2025-11-11 00:38:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: process.env.NODE_ENV === 'production' ? '/SEU-REPOSITORIO/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
